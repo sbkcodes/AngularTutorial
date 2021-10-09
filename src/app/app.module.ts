a3e0d6ed66a64d0a47080411351658c53c8d7d65
sbkcodes <sbkcodes@gmail.com>
2021-10-09 18:53:37 +0530

    remove parent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ParentComponent } from './parent.component';
 
 @NgModule({
-  declarations: [AppComponent, ParentComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/parent.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-parent',
-  template: '<h1>Child</h1>',
-})
-export class ParentComponent {}
