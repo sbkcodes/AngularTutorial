01ac30ca8161ef8180f6a0031da3df54d7c5e871
sbkcodes <sbkcodes@gmail.com>
2021-10-11 19:11:35 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'ITP CANDIDATE';
-  note: string = 'Good Morning';
-  imgUrl: string = 'www.abc.com/photo.jpeg';
-
-  add(): number {
-    return 3;
-  }
-
-  addData(): void {
-    console.log('addData');
-  }
-
-  addDataEvent(e: any): void {
-    console.log('event data', e);
-  }
 }
