b3df3e375e85a117bb180be703faf03aa6d6ea25
sbkcodes <sbkcodes@gmail.com>
2021-10-19 17:56:37 +0530

    css added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
 import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
